c182dd73a9b9403859fe7f58d8f86c0128668059
reubsinit <reubenmwilson@gmail.com>
2021-02-13 11:09:49 +1000

    new: add barebones Pet component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+const Pet = () => {
+  return React.createElement('div', {}, [
+    React.createElement('h1', {}, 'Big Dog'),
+    React.createElement('h2', {}, 'Dog'),
+    React.createElement('h2', {}, 'American Staffy'),
+  ]);
+};
+
 const App = () => {
-  return React.createElement(
-    'div',
-    {},
-    React.createElement('h1', {}, 'Adopt Me!')
-  );
+  return React.createElement('div', {}, [
+    React.createElement('h1', {}, 'Adopt Me!'),
+    React.createElement(Pet),
+    React.createElement(Pet),
+    React.createElement(Pet),
+  ]);
 };
 ReactDOM.render(
   React.createElement(App),
