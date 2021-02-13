b62559f009495ee6ff87177933bd841c433dbd10
reubsinit <reubenmwilson@gmail.com>
2021-02-13 15:03:17 +1000

    enhance: hit pets api for animal breeds

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { ANIMALS } from '@frontendmasters/pet';
+import React, { useState, useEffect } from 'react';
+import pet, { ANIMALS } from '@frontendmasters/pet';
 import useDropdown from './useDropdown';
 
 export default function SearchParams() {
@@ -12,7 +12,20 @@ export default function SearchParams() {
     'dog',
     ANIMALS
   );
-  const [breed, BreedDropdown] = useDropdown('Breed', '', breeds);
+  const [breed, BreedDropdown, setBreed] = useDropdown(
+    'Breed',
+    '',
+    breeds
+  );
+
+  useEffect(() => {
+    setBreeds([]);
+    setBreed('');
+    pet.breeds(animal).then(({ breeds }) => {
+      const breedNames = breeds.map(({ name }) => name);
+      setBreeds(breedNames);
+    }, console.error);
+  }, [animal, setBreed, setBreeds]);
 
   return (
     <div className="search-params">
