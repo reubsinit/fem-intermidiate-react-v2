3a8d0f9de0029a3528deb725327fa704514a9c7b
reubsinit <reubenmwilson@gmail.com>
2021-02-13 11:14:48 +1000

    enhance: add props to Pet component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
-const Pet = () => {
+const Pet = ({name, animal, breed}) => {
   return React.createElement('div', {}, [
-    React.createElement('h1', {}, 'Big Dog'),
-    React.createElement('h2', {}, 'Dog'),
-    React.createElement('h2', {}, 'American Staffy'),
+    React.createElement('h1', {}, name),
+    React.createElement('h2', {}, animal),
+    React.createElement('h2', {}, breed),
   ]);
 };
 
 const App = () => {
   return React.createElement('div', {}, [
     React.createElement('h1', {}, 'Adopt Me!'),
-    React.createElement(Pet),
-    React.createElement(Pet),
-    React.createElement(Pet),
+    React.createElement(Pet, {
+      name: 'Big Dog',
+      animal: 'Dog',
+      breed: 'American Staffy',
+    }),
+    React.createElement(Pet, {
+      name: 'Miso',
+      animal: 'Dog',
+      breed: 'Long Haired Chihauhau',
+    }),
+    React.createElement(Pet, {
+      name: 'Kim Chi',
+      animal: 'Cat',
+      breed: 'All Sorts!',
+    }),
   ]);
 };
 ReactDOM.render(
