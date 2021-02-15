5003ad784d1c872a136277c400b48af25245f330
reubsinit <reubenmwilson@gmail.com>
2021-02-15 15:28:40 +1000

    config: use component properties

--- src/Details.js
+++ src/Details.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import pet from '@frontendmasters/pet';
+import Carousel from './Carousel';
 
 export default class Details extends React.Component {
-  constructor(props) {
-    super(props);
+  state = { loading: true };
 
-    this.state = {
-      loading: true,
-    };
-  }
   componentDidMount() {
     pet.animal(this.props.id).then(({ animal }) => {
       this.setState({
@@ -26,9 +22,17 @@ export default class Details extends React.Component {
     if (this.state.loading) {
       return <h1>Loading ...</h1>;
     }
-    const { animal, breed, location, description, name } = this.state;
+    const {
+      animal,
+      breed,
+      location,
+      description,
+      name,
+      media,
+    } = this.state;
     return (
       <div className="details">
+        <Carousel media={media} />
         <div>
           <h1>{name}</h1>
           <h2>{`${animal} - ${breed} - ${location}`}</h2>
