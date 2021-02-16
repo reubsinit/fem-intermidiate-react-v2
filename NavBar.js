21ec599c72f1fac69de3a2c3c46715df9f9c8bc0
reubsinit <reubenmwilson@gmail.com>
2021-02-16 11:30:54 +1000

    config: add emotion for css prop

---
+++ NavBar.js
@@ -0,0 +1,41 @@
+/** @jsx jsx */
+import React from 'react';
+import { css, jsx, keyframes } from '@emotion/react';
+import { Link } from '@reach/router';
+import colors from './colors';
+
+const spin = keyframes`
+  to {
+    transform: rotate(360deg)
+  }
+`;
+
+const NavBar = () => {
+  return (
+    <header
+      css={css`
+        background-color: ${colors.dark};
+        padding: 15px;
+      `}
+    >
+      <Link to="/">Adopt Me!</Link>
+      <span
+        css={css`
+          font-size: 60px;
+          display: inline-block;
+          animation: 1s ${spin} linear infinite;
+          &:hover {
+            animation: 1s ${spin} linear reverse infinite;
+            text-decoration: underline;
+          }
+        `}
+        role="img"
+        aria-label="logo"
+      >
+        🥰
+      </span>
+    </header>
+  );
+};
+
+export default NavBar;
